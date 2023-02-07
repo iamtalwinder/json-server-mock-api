e1553a0b64c697d29a1352314bcb5967bf16130b
Talwinder Singh <singhtalwinder790@gmail.com>
2023-02-07 12:12:15 +0530

    feat: add env

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+require('dotenv').config();
+
+const env = process.env;
+
+module.exports = {
+  JWT_SECRET_KEY: env.JWT_SECRET_KEY,
+  DB_PATH: path.join(__dirname, './db/db.json'),
+};
+  
